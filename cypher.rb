604eefebf40d65a1fa07ab3de7b084ab6142bf54
L.Carquet <laetiticarquet@gmail.com>
2016-07-21 12:20:24 +0200

    decipher the encrypted message and print it with spaces

--- cypher.rb
+++ cypher.rb
@@ -1,15 +1,22 @@
 def solve_cipher(input, n)
 	split_into_an_array = input.split('')
 	shift = []
+
 	for letter in split_into_an_array
 		if letter == " "
-			shift << " "
+			shift << 0
 		else
 			shift << letter.ord + n
 		end
 	end
-	
-	print shift
+
+	shift.each do |let| let
+		if let == 0
+			print let = " "
+		else
+			print let = let.chr
+		end
+	end
 end
 
 solve_cipher("p| uhdo qdph lv grqdog gxfn", -3)
