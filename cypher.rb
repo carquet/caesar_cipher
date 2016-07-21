1eab841cf37dc8d4c4d0a068aea5184b5cefbc18
L.Carquet <laetiticarquet@gmail.com>
2016-07-21 11:12:41 +0200

    shift letter to n number

--- cypher.rb
+++ cypher.rb
@@ -1,7 +1,12 @@
-def solve_cipher(input)
+def solve_cipher(input, n)
 	split_into_an_array = input.split('')
+	split_into_an_array.each do |letter| puts letter.ord + n
+	
+
+	end
+	
 
 
 end
 
-print solve_cipher("ifmmp")
+print solve_cipher("ifmmp", -1)
